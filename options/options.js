2e77282e2500ac7dcce5d4535c93ffb5244a6d31
Crowdbotics <team@crowdbotics.com>
2024-03-20 15:05:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gregs-guitar-tuner-47160",
+  url: "https://gregs-guitar-tuner-47160.botics.co",
+  api: "https://gregs-guitar-tuner-47160.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
